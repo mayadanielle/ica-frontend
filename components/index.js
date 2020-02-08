648901adfe67bbec0d1bfa72876d4d0e0b357e18
Destiny Ajakaiye <destinyajakaiye@gmail.com>
2020-02-08 19:17:13 +0100

    Added global Head information

---
+++ components/index.js
@@ -0,0 +1,5 @@
+import Head from './Head/head';
+
+export {
+    Head
+};
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React, {Component,Fragment} from 'react';
+
+class HomePage extends Component {
+    render() {
+        return (
+            <Fragment>
+                <h1>Welcome to home page</h1>
+            </Fragment>
+        )
+    };
+};
+
+export default HomePage;
